13c41323ff799274afd970bb41ac27148a3e77a7
Raghu Reddy <raghureddy.ram@gmail.com>
2024-04-25 10:49:43 -0700

    Add method for rake task

--- app/models/task.rb
+++ app/models/task.rb
@@ -5,4 +5,14 @@ class Task < ApplicationRecord
 
     enum status: ["incomplete", "needing_attention"]
     enum type: ["feed", "bathe", "socialize"]
+
+
+    def mark_task_status
+        now = Time.zone.now 
+        self.current_interval = now - self.updated_at
+        if current_interval > self.scheduling_interval
+            self.status = "needing_attention"
+            self.save
+        end
+    end
 end

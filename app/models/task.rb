e0b0a34309c350be66ff7da35d2ba62624fd6bcf
Raghu Reddy <raghureddy.ram@gmail.com>
2024-04-25 10:55:39 -0700

    Add guard

--- app/models/task.rb
+++ app/models/task.rb
@@ -2,12 +2,14 @@ class Task < ApplicationRecord
     belongs_to :animal
     attribute :scheduling_interval, :interval
     validates_presence_of :name
+    validates_presence_of :type
 
     enum status: ["incomplete", "needing_attention"]
     enum type: ["feed", "bathe", "socialize"]
 
 
     def mark_task_status
+        return unless self.scheduling_interval.present?
         now = Time.zone.now 
         self.current_interval = now - self.updated_at
         if current_interval > self.scheduling_interval

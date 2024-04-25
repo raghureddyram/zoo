5f89098e1804009fecd9472b4fec88b9bc1d5e8c
Raghu Reddy <raghureddy.ram@gmail.com>
2024-04-25 15:33:40 -0700

    Update relationship

--- app/models/habitat.rb
+++ app/models/habitat.rb
@@ -2,9 +2,6 @@ class Habitat < ApplicationRecord
     include Notable
     include WithNotes
 
-    has_many :animals, dependent: :destroy
+    has_many :animals, dependent: :destroy, foreign_key: "primary_habitat_id"
     belongs_to :zoological_park
-
-
-    
 end
